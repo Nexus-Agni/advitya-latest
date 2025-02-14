f0d0fcf82ce58e51237a86b85db940941fab1d0a
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-02-14 10:57:04 +0530

    refactor Popup component; update close button formatting and image source

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -28,12 +28,15 @@ const Popup = ({ show, onClose }) => {
               className="bg-gray-50 relative dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] sm:w-[35rem] h-auto rounded-xl p-6 border-4 hover:border-purple-600 transition-all ease-in-out duration-300"
               onClick={handleViewMore}
             >
-              <button onClick={handleCloseClick} className="absolute top-2 right-2 z-10">
+              <button
+                onClick={handleCloseClick}
+                className="absolute top-2 right-2 z-10"
+              >
                 <X className="w-6 h-6 text-red-500" />
               </button>
               <div className="w-full mt-4">
                 <img
-                  src="https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67a5b7a0001971fd1f93/view?project=6756a8dc000c0d24754b&mode=admin"
+                  src="https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aed35c00280d45c76f/view?project=6756a8dc000c0d24754b&mode=admin"
                   height="1000"
                   width="1000"
                   className="h-full w-full object-cover rounded-xl"
