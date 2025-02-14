bd9b3377134197627a74ea53f7895579652ec29f
Leonardo Fernandes <leonardoschoolwork@gmail.com>
2025-02-14 14:18:52 +0530

    feat(Sponsors): add new sponsor logos to the component

--- src/components/Sponsors.jsx
+++ src/components/Sponsors.jsx
@@ -23,6 +23,70 @@ export function Sponsors() {
       id: 5,
       logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aef88a00250f76d44f/view?project=6756a8dc000c0d24754b&mode=admin",
     },
+    {
+      id: 6,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aeff8900057aa02a6f/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 7,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aeffbd00271878d38c/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 8,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aeffda001d8c293498/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 9,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aeffea002fd94bc704/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 10,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67aefffc002b22552fb1/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 11,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af0014001fb4a9dd9d/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 12,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af003a0007abf492ca/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 13,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af0048000e3a6f119a/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 14,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af00e9000336d97d20/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 15,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af00f100049fc4d56e/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 16,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af00fa0033bb2e9b50/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 17,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af010b0033864a9a38/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 18,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af011500073570f20a/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 19,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af011c0033f04cb64e/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 20,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af01230032a62183f5/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
+    {
+      id: 21,
+      logo: "https://cloud.appwrite.io/v1/storage/buckets/67a1d7b300237ca90544/files/67af012a000eb04f05ad/view?project=6756a8dc000c0d24754b&mode=admin",
+    },
   ];
 
   return (
